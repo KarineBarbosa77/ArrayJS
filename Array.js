eae005e3df9a03ceb15ac7a8351b72674b92c2c0
KarineBarbosa77 <160532475+KarineBarbosa77@users.noreply.github.com>
2024-03-27 18:27:11 +0000

    array

---
+++ Array.js
@@ -0,0 +1,20 @@
+
+var cores = [rosa, laranja, azul, verde, vermelho]
+
+
+var corEscolhida = cores[Math.floor(Math.random() * cores.length)]
+
+
+alert("Bem-vindo ao Jogo de Cores!\nTente adivinhar a cor escolhida.")
+
+while(true) {
+    
+    var palpite = prompt("Digite uma cor:")
+
+    if (palpite == corEscolhida) {
+        alert("Parabéns! Você acertou a cor: " + corEscolhida + ".")
+        break; 
+    } else {
+        alert("Tente novamente.")
+    }
+}
